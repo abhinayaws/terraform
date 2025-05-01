ef0b09713b9ed425f2b7e4e3a1090405630b594a
abhinay <abhinaydhanagari1@gmail.com>
2025-05-01 12:10:40 -0400

    day4 changes

---
+++ day4-backend/main.tf
@@ -0,0 +1,14 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
+  
+}
+resource "aws_subnet" "test" {
+    cidr_block = "10.0.0.0/24"
+    vpc_id = aws_vpc.myvpc.id
+  
+}
+resource "aws_subnet" "dev" {
+    cidr_block = "10.0.1.0/24"
+    vpc_id = aws_vpc.myvpc.id
+  
+}
